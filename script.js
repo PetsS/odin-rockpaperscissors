73f1d67bffa1877343b05b032e48a3871e44e669
Peter Szots <103681468+PetsS@users.noreply.github.com>
2025-03-05 23:01:31 +0100

    add playRound functio

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-console.log("Rock Paper Scissors Game");
-
 // global variables to track score
 let humanScore = 0;
 let computerScore = 0;
-
-// variables for choices
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+let roundCounter = 0;
 
 // Human's Choice
 function getHumanChoice() {
@@ -61,29 +56,95 @@ function getComputerChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
+  let whoScores = "";
+
   if (humanChoice === "rock") {
     if (computerChoice === "scissors") {
       humanScore++;
+      whoScores = "You Score!";
     } else if (computerChoice === "paper") {
       computerScore++;
+      whoScores = "Computer Scores!";
     }
   } else if (humanChoice === "paper") {
     if (computerChoice === "rock") {
       humanScore++;
+      whoScores = "You Score!";
     } else if (computerChoice === "scissors") {
       computerScore++;
+      whoScores = "Computer Scores!";
     }
   } else if (humanChoice === "scissors") {
     if (computerChoice === "paper") {
       humanScore++;
+      whoScores = "You Score!";
     } else if (computerChoice === "rock") {
       computerScore++;
+      whoScores = "Computer Scores!";
     }
   }
+
+  return whoScores;
 }
 
-// Final display in console
-playRound(humanSelection, computerSelection);
+function displayScore(
+  humanSelection,
+  computerSelection,
+  humanScore,
+  computerScore,
+  roundCounter
+) {
+  console.log(`---------- Round: ${roundCounter} ----------\n\n`);
+  console.log(`Your choice is: ${humanSelection}`);
+  console.log(`Computer's choice is: ${computerSelection}`);
+  console.table("-----------------------------");
+  console.log(`Your score is: ${humanScore}`);
+  console.log(`The Computer's score is: ${computerScore}`);
+  console.table("");
+}
 
-// console.log(`Your choice is: ${getHumanChoice()}`);
-// console.log(`Computer's choice is: ${getComputerChoice()}`);
+function playGame() {
+  console.log(
+    "Rock Paper Scissors Game\n" +
+      "The game will play over 5 rounds.\n" +
+      "Rock beats Scissors, Scissors beats Paper, Paper beats Rock."
+  );
+
+  while (roundCounter < 5) {
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+
+    // increment the round by 1
+    roundCounter++;
+
+    playRound(humanSelection, computerSelection);
+
+    displayScore(
+      humanSelection,
+      computerSelection,
+      humanScore,
+      computerScore,
+      roundCounter
+    );
+  }
+
+  console.log(`---------- Final Score: ----------\n\n`);
+  console.log(
+    `Final score is: ${humanScore} - ${computerScore}.`
+  );
+  if (humanScore > computerScore) {
+    console.log(
+      `Congratulations, You Win!`
+    );
+  } else if (humanScore < computerScore) {
+    console.log(
+      `Computer wins!`
+    );
+  } else {
+    console.log("It is a tie!");
+  }
+
+}
+
+// Final
+playGame();
