5ddba9c1bc7b74545103337b604ad4326de43aa0
Peter Szots <103681468+PetsS@users.noreply.github.com>
2025-03-05 21:34:42 +0100

    add human's choice function

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 console.log("Rock Paper Scissors Game");
 
-let randomNumber = Math.floor(Math.random() * 3) + 1;
-console.log(randomNumber);
 
+// Human's Choice
+
+function getHumanChoice() {
+  let choice = "";
+  let number = parseInt(
+    prompt(
+      "What is your choice? Enter a number between 1 and 3 (1 - rock, 2 - paper, 3 - scissors)"
+    )
+  );
+
+  switch (number) {
+    case 1:
+      choice = "rock";
+      break;
+    case 2:
+      choice = "paper";
+      break;
+    case 3:
+      choice = "scissors";
+      break;
+    default:
+      choice = "not a valid number";
+      break;
+  }
+
+  return choice;
+}
+
+// Computer's Choice
 function getComputerChoice() {
   let choice = "";
+
+  // Generate a random number
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
 
   switch (randomNumber) {
     case 1:
@@ -17,11 +47,15 @@ function getComputerChoice() {
       choice = "scissors";
       break;
     default:
-      choice = "no number";
+      choice = "error";
       break;
   }
 
   return choice;
 }
 
+
+
+// Final display in console
+console.log(`Your choice is: ${getHumanChoice()}`);
 console.log(`Computer's choice is: ${getComputerChoice()}`);
